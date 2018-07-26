f0b92d155016a1c93f5e2c25f3d09c00a5cb9692
Pablo Antonio Reyes Betanzo <pablo.reyes@lvaindices.com>
2018-07-26 15:16:55 -0400

    Add Webpack Dev Server config and Hot Module Replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    devServer: {
+        stats: "errors-only", // Display errors only.
+        host: process.env.HOST, // Default is localhost
+        port: process.env.PORT, // Defaults is 8080
+        open: true,
+        overlay: true // Enables errors to be shown directly in the browser window
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: "Webpack Development Boilerplate"
