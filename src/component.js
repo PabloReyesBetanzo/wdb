911cc46f475f1893c801664a1900a9a5f6f96c98
Pablo Antonio Reyes Betanzo <pablo.reyes@lvaindices.com>
2018-07-26 17:06:56 -0400

    Fix

--- src/component.js
+++ src/component.js
@@ -1,6 +1,7 @@
 export default (text = "<h1>Hello world</h1>") => {
     const element = document.createElement("div");
 
+    element.classList = "pure-button";
     element.innerHTML = text;
 
     return element;
