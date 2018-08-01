a04111951c19ee7afdc412950ec5b1662305395c
Pablo Antonio Reyes Betanzo <pablo.reyes@lvaindices.com>
2018-08-01 09:33:20 -0400

    jQuery test

--- dist/main.min.606b41221bb27abfc311.js
+++
@@ -1,3 +0,0 @@
-/*! 9dd7feb */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,e,o){},,function(n,e,o){"use strict";o.r(e);o(0);document.body.appendChild(function(){var n=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"<h1>Hello world</h1>",e=document.createElement("div");return e.innerHTML=n,e}())}],[[2,1]]]);
-//# sourceMappingURL=main.min.606b41221bb27abfc311.js.map
--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import component from "./component";
 import "./main.sass";
 
 document.body.appendChild(component());
+
+$(function() {
+    alert("Hello, World!");
+});
